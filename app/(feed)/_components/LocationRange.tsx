a021e16d5ba3dfae7c6599779857d4da051e3b70
Will <willgraham367@gmail.com>
2024-06-27 23:13:00 -0400

    build: added feed and viewing mileage button on navbar

---
+++ app/(feed)/_components/LocationRange.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+interface LocationRangeProps {
+    miles: number,
+    setMiles: (miles: number) => void
+}
+
+const LocationRange = ({ miles, setMiles }: LocationRangeProps) => {
+
+    const milesPlaceholder = `${miles} miles`;
+
+    const handleMilesChange = (miles: string) => {
+        setMiles(parseInt(miles));
+    }
+
+    return (
+        <div className="flex items-center gap-x-1">
+            <h3>
+                Viewing posts within:
+            </h3>
+            <Select onValueChange={handleMilesChange}>
+                <SelectTrigger className="w-[6.5rem]">
+                    <SelectValue placeholder={milesPlaceholder} />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="5">5 miles</SelectItem>
+                    <SelectItem value="10">10 miles</SelectItem>
+                    <SelectItem value="25">25 miles</SelectItem>
+                    <SelectItem value="50">50 miles</SelectItem>
+                    <SelectItem value="100">100 miles</SelectItem>
+                </SelectContent>
+            </Select>
+        </div>
+    )   
+}
+
+export default LocationRange
---
+++ app/(feed)/_components/Navbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { ModeToggle } from "@/components/theme-mode-toggle"
+import Logo from "./Logo"
+import { Button } from "@/components/ui/button"
+import LocationRange from "./LocationRange";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Ellipsis } from "lucide-react";
+import { useState } from "react";
+
+const Navbar = () => {
+
+    const [miles, setMiles] = useState(20);
+
+    return (
+        <div className="z-50 bg-black fixed top-0 flex items-center justify-between w-full px-4 py-2">
+            <Logo />
+            <span className="hidden sm:block">
+                <LocationRange miles={miles} setMiles={setMiles}/>
+            </span>
+            <div className="flex items-center gap-x-2">
+                <Button>
+                    Log in
+                </Button>
+
+                <span className="hidden sm:block">
+                    <ModeToggle />
+                </span>
+
+                <span className="flex sm:hidden items-center">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Ellipsis size={32} className="" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Options</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <p className="pr-[14px]">Appearance theme: </p>
+                                <ModeToggle />
+                            </DropdownMenuItem>
+
+                            <DropdownMenuItem>
+                                <LocationRange miles={miles} setMiles={setMiles} />
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </span>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
