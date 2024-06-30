4ed5d91690a87eea8e27aa27b79e6c15f4a66027
Will <willgraham367@gmail.com>
2024-06-30 17:21:23 -0400

    build: added new create page for users to make posts

--- client/app/(feed)/_components/MainContent.tsx
+++ client/app/(feed)/_components/MainContent.tsx
@@ -1,12 +1,33 @@
+import { useEffect } from "react"
 import Post from "./Post"
 
-const MainContent = () => {
+const MainContent = async () => {
+
+    const getPosts = async () => {
+        try {
+            const res = await fetch('http://localhost:8080/posts')
+            return res.json()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const posts = await getPosts();
+    console.log(posts)
+
     return (
         <div className="w-full flex flex-col items-center gap-y-4">
             
             <Post />
             <Post />
             <Post />
+            {
+                posts.map((post: any) => {
+                    return (
+                        <h1>{post.title}</h1>
+                    )
+                })
+            }
         </div>
     )
 }
