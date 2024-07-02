d5939a309f6c305adc0bb44974e6035809888bc2
Will <willgraham367@gmail.com>
2024-07-02 01:48:52 -0400

    style: spacing and border below create title

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -4,12 +4,17 @@ import PostForm from "./_components/PostForm"
 const CreatePostPage = () => {
     return (
         <div className="flex justify-center w-full">
-            <div className="w-[100%] md:w-[60%] md:bg-neutral-100 md:dark:bg-neutral-800 md:rounded-xl p-6">
-                <h1 className="text-2xl font-medium">
-                    Create Post
-                </h1>
+            <div className="w-[100%] md:w-[60%] md:bg-neutral-100 md:dark:bg-neutral-800 md:rounded-xl">
+                
+                <div className="w-full border-b-2 border-neutral-200 dark:border-neutral-700 px-6 pt-6 pb-3">
+                    <h1 className="text-2xl font-bold">
+                        Create Post
+                    </h1>
+                </div>
 
-                <PostForm />    
+                <div className="p-6">
+                    <PostForm />    
+                </div>
             </div>
         </div>
     )
