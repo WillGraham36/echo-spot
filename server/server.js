8c9fb7c06985bb86ee91ac38c9b4cd55bd4f1d5a
Will <willgraham367@gmail.com>
2024-07-04 00:50:02 -0400

    refactor: removed unecessary comment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import connectToDB from './database.js';
 import cors from 'cors';
 
-// import subscribersRouter from './routes/subscribers.js';
 import postsRouter from './routes/posts.js';
 
 const app = express();
