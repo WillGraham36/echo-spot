0cc7a25d13110ce010a4fca036027fc12fb34db1
Will <willgraham367@gmail.com>
2024-07-14 15:41:44 -0400

    build: feed fetches feed from server

--- client/app/(feed)/_components/MainContent.tsx
+++ client/app/(feed)/_components/MainContent.tsx
@@ -1,28 +1,42 @@
-import { useEffect } from "react"
+"use client";
+import { use, useEffect, useState } from "react"
 import Post from "./Post"
+import useLocation from "@/hooks/useLocation"
 
-const MainContent = async () => {
+const MAX_DIST = 10000000000; //in meters
+const limit = 100;
+const offset = 0;
 
-    const getPosts = async () => {
-        try {
-            const res = await fetch('http://localhost:8080/posts')
-            return res.json()
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
-    const posts = await getPosts();
-    console.log(posts)
+const MainContent =  () => {
+
+    const [posts, setPosts] = useState<any[]>([]);
+    
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const location = await useLocation();
+            if (location) {
+                try {
+                    const res = await fetch(`http://localhost:8080/posts/feed?lat=${location.lat}&long=${location.long}&maxDistance=${MAX_DIST}&limit=${limit}&offset=${offset}`);
+                    const data = await res.json();
+                    setPosts(data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        };
+
+        fetchPosts();
+    }, []);
 
     return (
         <div className="w-full flex flex-col items-center gap-y-4">
-            
-
             {
+
                 posts.map((post: any) => {
                     return (
                         <Post 
+                            key={post._id}
                             postId={post.postId}
                             userId={post.userId}
                             date={post.date}
