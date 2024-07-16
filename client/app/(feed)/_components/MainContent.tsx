9971704bcfb8a123b82aba3f6274b578302f08d5
Will <willgraham367@gmail.com>
2024-07-16 00:39:17 -0400

    build: added error state for failing to fetch posts

--- client/app/(feed)/_components/MainContent.tsx
+++ client/app/(feed)/_components/MainContent.tsx
@@ -12,21 +12,25 @@ const offset = 0;
 const MainContent =  () => {
 
     const [posts, setPosts] = useState<any[]>([]);
-    const [loadingPosts, setLoadingPosts] = useState<boolean>(true);
     const [viewRadius, setViewRadius] = useState<number>(20);
+
+    const [postsStatus, setPostsStatus] = useState({
+        loading: true,
+        error: false
+    });
     
     useEffect(() => {
         const fetchPosts = async () => {
-            setLoadingPosts(true);
+            setPostsStatus({ loading: true, error: false });
             const location = await useLocation();
             if (location) {
                 try {
                     const res = await fetch(`http://localhost:8080/posts/feed?lat=${location.lat}&long=${location.long}&maxDistance=${viewRadius * 1609}&limit=${limit}&offset=${offset}`);
                     const data = await res.json();
                     setPosts(data);
-                    setLoadingPosts(false);
+                    setPostsStatus({ loading: false, error: false });
                 } catch (error) {
-                    console.log(error);
+                    setPostsStatus({ loading: false, error: true });
                 }
             }
         };
@@ -34,6 +38,25 @@ const MainContent =  () => {
         fetchPosts();
     }, [viewRadius]);
 
+    if(postsStatus.error) {
+        return (
+            <div className="text-2xl text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5">
+                <h1>
+                    An error has occured, please refresh the page
+                </h1>
+            </div>
+        )
+    }
+    if(postsStatus.loading) {
+        return (
+            <>
+                <div className="absolute top-24">
+                    <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
+                </div>
+                <Spinner size={"large"} className="mt-5" />
+            </>
+        )
+    }
     return (
         <>
             <div className="absolute top-24"> 
@@ -41,31 +64,28 @@ const MainContent =  () => {
             </div>
             <div className="w-full flex flex-col items-center gap-y-4">
                 {
-                    loadingPosts ? 
-                        <Spinner size={"large"} className="mt-5"/> 
-                    :
-                        posts.length > 0 ? posts.map((post: any) => {
-                            return (
-                                <Post
-                                    key={post._id}
-                                    postId={post.postId}
-                                    userId={post.userId}
-                                    date={post.date}
-                                    category={post.category}
-                                    title={post.title}
-                                    upvotes={post.upvotes}
-                                />
-                            )
-                        }) :
-                            <div className="text-2xl text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5">
-                                <h1>
-                                    Looks like there's no echos near you...
-                                </h1>
-                                <h1 className="pt-2">
-                                    Try increasing your view radius or create one yourself!
-                                </h1>
-                            </div>
-                    }
+                    posts.length > 0 ? posts.map((post: any) => {
+                        return (
+                            <Post
+                                key={post._id}
+                                postId={post.postId}
+                                userId={post.userId}
+                                date={post.date}
+                                category={post.category}
+                                title={post.title}
+                                upvotes={post.upvotes}
+                            />
+                        )
+                    }) :
+                        <div className="text-2xl text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5">
+                            <h1>
+                                Looks like there's no echos near you...
+                            </h1>
+                            <h1 className="pt-2">
+                                Try increasing your view radius or create one yourself!
+                            </h1>
+                        </div>
+                }
             </div>
         </>
     )
