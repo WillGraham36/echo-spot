81b901b50d1a6082276411b4fbc9a7e0e8959ef9
Will <willgraham367@gmail.com>
2024-07-21 18:50:36 -0400

    build: added post page and link from each post in the main feed

--- client/app/(feed)/_components/Post.tsx
+++ client/app/(feed)/_components/Post.tsx
@@ -12,6 +12,7 @@ import { Button } from "@/components/ui/button"
 import CalculteTimeDiff from "@/utils/CalculteTimeDiff"
 import { useState } from "react"
 import UpvotesButtons from "./Upvotes"
+import Link from "next/link"
 
 interface Comment {
     commentId: string,
@@ -56,70 +57,74 @@ const Post = ({
     upvotes = upvotes ? upvotes : 0;
     const [numUpvotes, setNumUpvotes] = useState(upvotes);
 
-    return (    
-        <div className="w-full md:w-[70%] md:bg-neutral-100 md:dark:bg-neutral-800 md:rounded-xl md:p-4 px-4 pt-1 pb-3 border-b md:border-y-0 border-y-muted-foreground">
-            <div className="flex justify-between items-center">
-                <span className=" inline-flex items-center gap-x-2">
-                    <Image
-                        src="/logo.png"
-                        alt="EchoSpot Logo"
-                        width={40}
-                        height={40}
-                        className="hidden md:block dark:bg-primary rounded-xl"
-                    />
-                    <h3 className="font-bold">
-                        {category}
-                    </h3>
-                    <h2 className="text-muted-foreground text-sm">
-                        {CalculteTimeDiff({compToDate: new Date(date)})}
-                    </h2>
-                </span>
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <Ellipsis size={24} />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>Options</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
+    const handleButtonClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+    }
 
-                        <DropdownMenuItem>
-                            {/* TODO: Add block user feature */}
-                            Block User
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
+    return (
+        <Link href={`post/${_id}`} className="w-full md:w-[70%] md:bg-neutral-100 md:dark:bg-neutral-800 md:rounded-xl md:p-4 px-4 pb-3 border-b md:border-y-0 border-y-muted-foreground pt-1">
+                <div className="flex justify-between items-center">
+                    <span className=" inline-flex items-center gap-x-2">
+                        <Image
+                            src="/logo.png"
+                            alt="EchoSpot Logo"
+                            width={40}
+                            height={40}
+                            className="hidden md:block dark:bg-primary rounded-xl"
+                        />
+                        <h3 className="font-bold">
+                            {category}
+                        </h3>
+                        <h2 className="text-muted-foreground text-sm">
+                            {CalculteTimeDiff({ compToDate: new Date(date) })}
+                        </h2>
+                    </span>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger onClick={handleButtonClick}>
+                            <Ellipsis size={24} />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Options</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
 
-
-            <div className="text-start pt-2 md:pl-12">
-                <h1>
-                    {title}
-                </h1>
-            </div>
-
-            <div className="flex md:pl-12 pt-3 justify-between">
-                <div className="flex items-center gap-x-2">
-                    <Button size={"postBtn"} variant={"ghostHover"}>
-                        <MessageCircle size={26} />
-                        <p className="font-medium px-2">
-                            {comments ? comments.length : 0}
-                        </p>
-                    </Button>
-                    <Button size={"postBtn"} variant={"ghostHover"}>
-                        <Forward size={26} />
-                    </Button>
-                    
-                    
+                            <DropdownMenuItem>
+                                {/* TODO: Add block user feature */}
+                                Block User
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
-                    <UpvotesButtons 
-                        upvotes={numUpvotes} 
+
+
+                <div className="text-start pt-2 md:pl-12">
+                    <h1>
+                        {title}
+                    </h1>
+                </div>
+
+                <div className="flex md:pl-12 pt-3 justify-between">
+                    <div className="flex items-center gap-x-2">
+                        <Button size={"postBtn"} variant={"ghostHover"} onClick={handleButtonClick}>
+                            <MessageCircle size={26} />
+                            <p className="font-medium px-2">
+                                {comments ? comments.length : 0}
+                            </p>
+                        </Button>
+                        <Button size={"postBtn"} variant={"ghostHover"} onClick={handleButtonClick}>
+                            <Forward size={26} />
+                        </Button>
+
+
+                    </div>
+                    <UpvotesButtons
+                        upvotes={numUpvotes}
                         setUpvotes={setNumUpvotes}
                         postId={_id}
                         usersWhoUpvoted={usersWhoUpvoted}
                         usersWhoDownvoted={usersWhoDownvoted}
-                    />  
-            </div>
-        </div>
+                    />
+                </div>
+        </Link>
     )
 }
 
