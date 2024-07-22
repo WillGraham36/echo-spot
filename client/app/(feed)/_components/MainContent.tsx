800baa5663d0f8867c7eba93907cf9c76a35c618
Will <willgraham367@gmail.com>
2024-07-22 01:15:08 -0400

    style: loading spinner always appears on bottom of posts

--- client/app/(feed)/_components/MainContent.tsx
+++ client/app/(feed)/_components/MainContent.tsx
@@ -5,15 +5,16 @@ import useLocation from "@/hooks/useLocation"
 import LocationRange from "./LocationRange";
 import { Spinner } from "@/components/ui/spinner";
 import { PostType } from "@/types/PostType";
+import { Button } from "@/components/ui/button";
 
-const limit = 100;
-const offset = 0;
+const limit = 8;
 
 
 const MainContent =  () => {
 
     const [posts, setPosts] = useState<PostType[]>([]);
     const [viewRadius, setViewRadius] = useState<number>(20);
+    const [offset, setOffset] = useState<number>(0);
 
     const [postsStatus, setPostsStatus] = useState({
         loading: true,
@@ -22,13 +23,13 @@ const MainContent =  () => {
     
     useEffect(() => {
         const fetchPosts = async () => {
-            setPostsStatus({ loading: true, error: false });
+            setPostsStatus({ loading: (offset > 0), error: false });
             const location = await useLocation();
             if (location) {
                 try {
                     const res = await fetch(`http://localhost:8080/posts/feed?lat=${location.lat}&long=${location.long}&maxDistance=${viewRadius * 1609}&limit=${limit}&offset=${offset}`);
                     const data = await res.json();
-                    setPosts(data);
+                    setPosts([...posts, ...data]);
                     setPostsStatus({ loading: false, error: false });
                 } catch (error) {
                     setPostsStatus({ loading: false, error: true });
@@ -37,7 +38,7 @@ const MainContent =  () => {
         };
 
         fetchPosts();
-    }, [viewRadius]);
+    }, [viewRadius, offset]);
 
     if(postsStatus.error) {
         return (
@@ -48,40 +49,38 @@ const MainContent =  () => {
             </div>
         )
     }
-    if(postsStatus.loading) {
-        return (
-            <>
-                <div className="absolute top-24">
-                    <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
-                </div>
-                <Spinner size={"large"} className="mt-28" />
-            </>
-        )
-    }
+    
     return (
         <>
             <div className="absolute top-24"> 
                 <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
+                <Button className="mt-4" onClick={() => setOffset(offset + limit)}>Load More</Button>
             </div>
             <div className="w-full flex flex-col items-center gap-y-4 pt-20">
-                {
-                    posts.length > 0 ? posts.map((post: any) => {
+                
+                {posts.length > 0 && (posts.map((post: any) => {
                         return (
                             <Post
                                 key={post._id}
                                 post={post}
                             />
                         )
-                    }) :
-                        <div className="text-2xl dark:text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5">
-                            <h1>
-                                Looks like there's no echos near you...
-                            </h1>
-                            <h1 className="pt-2">
-                                Try increasing your view radius or create one yourself!
-                            </h1>
-                        </div>
+                    }))
                 }
+
+                {postsStatus.loading ?
+                <>
+                    <div className="absolute top-24">
+                        <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
+                    </div>
+                    <Spinner size={"large"} className="mt-28" />
+                </> : (
+                    posts.length === 0 && 
+                        <div className="text-2xl dark:text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5">
+                            <h1>Looks like there's no echos near you...</h1>
+                            <h1 className="pt-2">Try increasing your view radius or create one yourself!</h1>
+                        </div>
+                )}
             </div>
         </>
     )
