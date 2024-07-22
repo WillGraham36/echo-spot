56301b91fa5ff775c753ac23ba103ae05f0e3a90
Will <willgraham367@gmail.com>
2024-07-21 21:50:14 -0400

    style: added padding to bottom

--- client/app/(feed)/page.tsx
+++ client/app/(feed)/page.tsx
@@ -4,7 +4,7 @@ import MainContent from "./_components/MainContent"
 const FeedPage = () => {
     return (
         <div className="min-h-full flex flex-col">
-            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1">
+            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 pb-10">
                 <MainContent />
             </div>
 
