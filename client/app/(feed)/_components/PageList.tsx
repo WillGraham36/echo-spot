c56dcf7d7c85c8caa5fb2bd5e277abccce7ef814
Will <willgraham367@gmail.com>
2024-07-23 02:02:19 -0400

    build: added infinite scroll to feed page

--- client/app/(feed)/_components/PageList.tsx
+++ client/app/(feed)/_components/PageList.tsx
@@ -5,9 +5,9 @@ import LocationRange from "./LocationRange";
 import { Spinner } from "@/components/ui/spinner";
 import { PostType } from "@/types/PostType";
 import { Button } from "@/components/ui/button";
-import { API_URL } from "@/utils/constants";
 import PostCard from "./PostCard";
 import { getPosts } from "@/actions/getPosts";
+import { useInView } from "react-intersection-observer";
 
 const limit = 8;
 
@@ -17,34 +17,59 @@ const PageList =  () => {
     const [posts, setPosts] = useState<PostType[]>([]);
     const [viewRadius, setViewRadius] = useState<number>(20);
     const [offset, setOffset] = useState<number>(0);
-
     const [postsStatus, setPostsStatus] = useState({
         loading: true,
         error: false,
-        noPostsFound: false
+        noMorePosts: false
     });
+    const [scrollTrigger, isInView] = useInView();
     
+    /**
+     * Fetch posts on initial render as well as when the view radius changes
+     */
     useEffect(() => {
         const fetchPosts = async () => {
             const location = await useLocation();
-            setPostsStatus({ loading: (offset > 0), error: false, noPostsFound: false });
+            setPostsStatus({ loading: true, error: false, noMorePosts: false });
             if (location) {
                 try {
                     const data = await getPosts({ viewRadius, offset, location, limit });
                     setPosts([...posts, ...data]);
-                    setPostsStatus({ loading: false, error: false, noPostsFound: data.length === 0});
+                    setPostsStatus({ loading: false, error: false, noMorePosts: data.length === 0});
                 } catch (error) {
-                    setPostsStatus({ loading: false, error: true, noPostsFound: false });
+                    setPostsStatus({ loading: false, error: true, noMorePosts: false });
                 }
             }
         };
-
         fetchPosts();
-    }, [viewRadius, offset]);
+    }, [viewRadius]);
 
-    const loadMorePosts = () => {
+    /**
+     * Loads more posts when the user scrolls to the bottom of the page
+     */
+    const loadMorePosts = async () => {
+        if(postsStatus.noMorePosts) return;
+
+        const location = await useLocation();
         setOffset((prevOffset) => prevOffset + limit);
+        if (location) {
+            try {
+                const data = await getPosts({ viewRadius, offset: offset + limit, location, limit });
+                setPosts((prevPosts) => [...prevPosts, ...data]);
+                setPostsStatus({ loading: false, error: false, noMorePosts: data.length < limit });
+            } catch (error) {
+                setPostsStatus({ loading: false, error: true, noMorePosts: false });
+            }
+        }
     }
+    /**
+     * This useEffect is needed so that the loadMorePosts function is called when the user scrolls to the bottom of the page
+     */
+    useEffect(() => {
+        if(isInView && !postsStatus.noMorePosts && !postsStatus.loading) {
+            loadMorePosts();
+        }
+    }, [isInView, !postsStatus.noMorePosts]);
 
     if(postsStatus.error) {
         return (
@@ -55,12 +80,26 @@ const PageList =  () => {
             </div>
         )
     }
-    if(postsStatus.noPostsFound) {
+    
+    /**
+     * Show message when there are no posts to display
+     */
+    if(postsStatus.noMorePosts && offset === 0) {
         return (
             <div className="text-2xl dark:text-white w-full md:w-[70%] bg-neutral-100 dark:bg-neutral-800 rounded-xl border-y-muted-foreground p-5 mt-20">
                 <h1>Looks like there's no echos near you...</h1>
                 <h1 className="pt-2">Try increasing your view radius or create one yourself!</h1>
             </div>
+        )
+    }
+    if(postsStatus.loading) {
+        return (
+            <>
+                <div className="absolute top-24">
+                    <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
+                </div>
+                <Spinner size={"large"} className="mt-28" />
+            </>
         )
     }
     
@@ -70,25 +109,22 @@ const PageList =  () => {
                 <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
             </div>
             <div className="w-full flex flex-col items-center gap-y-4 pt-20">
-                
-                {!postsStatus.loading ? (
-                    posts.map((post: any) => {
+                {posts.map((post: any) => {
                         return (
                             <PostCard
                                 key={post._id}
                                 post={post}
                             />
                         )
-                    })
-                ) : (
-                    <>
-                        <div className="absolute top-24">
-                            <LocationRange viewRadius={viewRadius} setViewRadius={setViewRadius} />
+                    })}
+                {/* <Button onClick={loadMorePosts} className="mt-10">Load More</Button> */}
+                <div>
+                    {!postsStatus.noMorePosts && 
+                        <div ref={scrollTrigger}>
+                            <Spinner size={"large"} className="mt-28" />
                         </div>
-                        <Spinner size={"large"} className="mt-28" />
-                    </>
-                )}
-                <Button onClick={loadMorePosts} className="mt-10">Load More</Button>
+                    }
+                </div>
             </div>
         </>
     )
