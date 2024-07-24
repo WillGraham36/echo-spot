049ac0a2b4c5df2654901c46d04eacbb4ecf7612
Will <willgraham367@gmail.com>
2024-07-24 01:11:28 -0400

    build: post page now displays the post card

--- client/app/(feed)/post/[postId]/page.tsx
+++ client/app/(feed)/post/[postId]/page.tsx
@@ -1,8 +1,17 @@
+import { getPost } from "@/actions/getPosts"
+import PostCard from "../../_components/PostCard";
 
-const PostPage = ({ params }: { params: { postId: string } }) => {
+const PostPage = async ({ params }: { params: { postId: string } }) => {
+
+    const postId = params.postId
+    const post = await getPost(postId);
+    console.log(post)
+
     return (
-        <div>
-            {params.postId}
+        <div className="w-full flex flex-col items-center gap-y-4 pt-8">
+            <div className="w-full md:w-[70%] md:bg-neutral-100 md:dark:bg-neutral-800 md:rounded-xl md:p-4 px-4 pb-3 border-b md:border-y-0 border-y-muted-foreground pt-1">
+                <PostCard post={post}/>
+            </div>
         </div>
     )
 }
