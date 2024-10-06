77cf03c03bd3372504a6877a6f903c2953ad1c13
Will <willgraham367@gmail.com>
2024-10-06 19:52:13 -0400

    Updated cors

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ import feedRouter from './routes/feed.js';
 import usersRouter from './routes/users.js';
 
 const app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const PORT = process.env.PORT || 8080;
 connectToDB();
 const corsOptions = {
